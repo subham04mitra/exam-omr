95d6cc25c0c099d00fbccc1364c057cfc451e7f3
subham04mitra <subhammitra58@gmail.com>
2025-08-31 20:56:23 +0530

    cors

--- src/main/java/com/exam/ExamApplication.java
+++ src/main/java/com/exam/ExamApplication.java
@@ -2,12 +2,28 @@ package com.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                .allowedOrigins("http://localhost:5173") // React dev server
+	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                .allowedHeaders("*")
+	                .allowCredentials(true); // only if you send cookies/Authorization headers
+	        }
+	    };
 	}
 
 }
